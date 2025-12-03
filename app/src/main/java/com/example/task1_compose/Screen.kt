47f5601b0a38694e7f81e65e267709493b40b19d
OleksandrLysenkoKPI <oleksandrlysenkotb23@gmail.com>
2025-12-03 22:28:28 +0200

    Revert "Added task 6" and added it properly
    This reverts commit 0f794d94f79e5df385f966014b340b8026d24620.

--- app/src/main/java/com/example/task1_compose/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.example.task1_compose
-
-sealed class Screen(val route: String) {
-    object Home: Screen(route = "home_screen")
-    object FuelCalculator: Screen(route = "fuel_calculator_screen")
-    object OilCalculator: Screen(route = "oil_calculator_screen")
-}
